8f21631a59a59b952ff81591cbd0c9ca6bededd5
dougsko <dougtko@gmail.com>
2009-09-11 02:16:29 -0400

    created a lib and wrote tests

--- lib/imagebin.rb
+++ lib/imagebin.rb
@@ -5,10 +5,45 @@
 
 require 'rubygems'
 require 'httpclient'
+require 'hpricot'
 
 class Imagebin
+    # This method takes a hash containing the variables in the POST
+    # request.
+    #
+    #   ibin = Imagebin.new( { "t" => "file",
+    #                          "name" => "",
+    #                          "tags" => "",
+    #                          "description" => "",
+    #                          "adult" => "f",
+    #                          "f" => "file_path",
+    #                        }) 
+    #
     def initialize(options)
-        @clnt = HTTPClient.new("http://imagebin.ca/upload.php")
-        yield @clnt.post('http://imagebin.ca/upload.php', options).content.match(/http.+?\.html/)
+        File.open(options["f"]) do |file|
+            options["f"] = file
+            clnt = HTTPClient.new
+            res = clnt.post('http://imagebin.ca/upload.php', options).content
+            @doc = Hpricot(res)
+        end
+    end
+
+    # Returns the link to the HTML site of the post.
+    #
+    #   ibin.site_link    #=> "http://imagebin.ca/view/xxxxxxx.html"
+    #
+    def site_link
+        @doc.at("a").innerHTML
+    end
+
+    # Returns the direct link to the dile posted.
+    #
+    #   ibin.pic_link    #=> "http://imagebin.ca/img/xxxxxxx.jpg"
+    #
+    def pic_link
+        clnt = HTTPClient.new
+        res = clnt.get(@doc.at("a").innerHTML).content
+        doc = Hpricot(res)
+        doc.at("#theimg")["src"]
     end
 end
--- spec/imagebin_spec.rb
+++ spec/imagebin_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe "Imagebin" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+    before do
+        options = { "t" => "file",
+                    "name" => "",
+                    "tags" => "",
+                    "description" => "",
+                    "adult" => "t",
+                    "f" => "spec/ZGrass04.jpg",
+        }
+        @ibin = Imagebin.new(options)
+    end
+
+    it "should return a link to the paste's website" do
+        link = @ibin.site_link
+        link.should match(/html/)
+    end
+
+    it "should return a link to the picture file" do
+        link = @ibin.pic_link
+        link.should match(/jpg/)
+    end
 end
