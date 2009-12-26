e3787d01554ad780746231d87e6a8282a500d827
dougsko <dougtko@gmail.com>
2009-12-26 17:16:52 -0500

    bin/takeScreenshot.sh

--- bin/takeScreenshot.sh
+++ bin/takeScreenshot.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+#
+# Take a screenshot, upload it, and display the link
+#
+# Requires: imagemagick, xmessage, and the imagebin script
+#
 
 # Location to save files
-WORKINGDIR=$HOME/screenshots
+WORKINGDIR=/tmp
 # Default delay before taking screenshots
 DELAY=3
 #Prefix to use for images captured. 
 PREFIX=screenshot
+IMPORT=`which import`
 
 # Check if the dir to store the screenshots exists, else create it: 
 if [ ! -d "${WORKINGDIR}" ]; then mkdir "${WORKINGDIR}"; fi 
@@ -14,5 +20,6 @@ i=`ls -l $WORKINGDIR/$PREFIX*.png | wc -l`
 ((i = i+1))
 
 sleep $DELAY
-import -window root $WORKINGDIR/$PREFIX-$i.png
-zenity --info --text=`imagebin -p -f $WORKINGDIR/$PREFIX-$i.png`
+$IMPORT -window root $WORKINGDIR/$PREFIX-$i.png
+xmessage -nearmouse `imagebin -p -l -f $WORKINGDIR/$PREFIX-$i.png`
+rm $WORKINGDIR/$PREFIX-$i.png
